65199faa5e291157dd931918a9ad7d91b3c0aa5f
satotakumi <sato.t.5970@gmail.com>
2024-12-07 12:09:40 +0900

    Initial Commit

---
+++ PunctualTests/PunctualTests.swift
@@ -0,0 +1,17 @@
+//
+//  PunctualTests.swift
+//  PunctualTests
+//
+//  Created by 佐藤匠 on 2024/12/07.
+//
+
+import Testing
+@testable import Punctual
+
+struct PunctualTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
